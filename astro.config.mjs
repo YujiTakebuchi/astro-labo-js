cafed1628bc36536c07ff499eade070e2287b127
YujiTakebuchi <takeyou.swift@gmail.com>
2023-03-28 00:52:15 +0900

    Pictureコンポーネントのインラインインポート試した

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://yujitakebuchi.github.io",
+  base: "/astro-labo-js",
   integrations: [image()],
 });
